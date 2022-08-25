4f0c4af219023f3ff97cd83c1c3e5c7fe6b3d09b
Shamsur <shamsur314@gmail.com>
2022-08-26 00:25:41 +0300

    added picture

--- pages/_document.js
+++ pages/_document.js
@@ -5,23 +5,10 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/static/favicon/apple-touch-icon.png"
-          />
-          <link
             rel="icon"
             type="image/png"
-            sizes="32x32"
-            href="/static/favicon/favicon-32x32.png"
+            href="/static/favicon/favicon.png"
           />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/static/favicon/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/static/favicon/site.webmanifest" />
         </Head>
         <body>
           <Main />
